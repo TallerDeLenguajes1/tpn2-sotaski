f6b46ced88ec40e7b68518627bc90f9c5b7c9ed4
celeste <mesientofuerte22@gmail.com>
2020-04-06 01:14:20 -0300

    punto 1 del tp2

---
+++ tp2_1_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 4
+#define M 5
+Int f,c;
+Double mt[N][M];
+…
+for(f = 0;f<N; f++)
+{
+ for(f = 0;f<N; f++)
+{
+cprintf(“%lf ”, mt[f][c]);
+}
+cprintf(“\n”);
+}
